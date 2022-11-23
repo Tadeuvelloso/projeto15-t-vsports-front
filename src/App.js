81e283e46b822ff65457e6fc068110882ea40d65
Tadeuvelloso <tadeuvellososilva@gmail.com>
2022-11-23 18:36:51 -0300

    feat: Routes & pages files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyle from "./components/GlobalStyle";
 import { CustomerProvider } from "./contexts/customer";
+import SignIn from "./components/SigninPage";
+import SignUp from "./components/SignupPage";
+import Cart from "./components/CartPage"; 
 
 function App() {
   return (
@@ -8,11 +11,11 @@ function App() {
       <GlobalStyle />
       <CustomerProvider>
         <Routes>
-          <Route />
-          <Route />
-          <Route />
-          <Route />
-          <Route />
+          <Route path="/signin" element={<SignIn />}  />
+          <Route path="/signup" element={<SignUp />}/>
+          <Route path="/home" element={<HomePage />}/>
+          <Route path="/cart" element={<Cart />}/>
+          <Route path="/checkout" element={<CheckOut />}/>
         </Routes>
       </CustomerProvider>
     </BrowserRouter>
