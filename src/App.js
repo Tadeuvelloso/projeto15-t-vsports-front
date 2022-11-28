e7f425aa135bca5f635e64501a7716b1b798137e
Tadeuvelloso <tadeuvellososilva@gmail.com>
2022-11-27 21:17:05 -0300

    fix: design homePage

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import SignUp from "./pages/SignupPage";
 import Cart from "./pages/CartPage"; 
 import CheckOut from "./pages/CheckoutPage";
 import HomePage from "./pages/HomePage";
+import GlobalHeader from "./pages/components/GlobalHeader";
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <CustomerProvider>
+      <GlobalHeader />
         <Routes>
           <Route path="/signin" element={<SignIn />}  />
           <Route path="/signup" element={<SignUp />}/>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import { CustomerContext } from "../contexts/customer"
@@ -7,39 +7,26 @@ import GlobalHeader from "./components/GlobalHeader"
 import ProductContainer from "./components/ProductContainer"
 
 export default function HomePage() {
-    const {myCartArray, token } = useContext(CustomerContext)
+    const { myCartArray, token } = useContext(CustomerContext)
     
     const navigate = useNavigate()
+    const [products, setProducts] = useState([]);
     
-    const arrayToTest = [
-        {
-            id: 1234,
-            productName: "t-shirt",
-            description: "great t-shirt",
-            brand: "nike",
-            category: "G",
-            price: 30.00,
-            country: "Brasil"
-        },
-        {
-            id: 12353,
-            productName: "t-shirt2",
-            description: "not great t-shirt",
-            brand: "par",
-            category: "M",
-            price: 20.00,
-            country: "EUA"
-        },
-        {
-            id: 234234,
-            productName: "t-shirt3",
-            description: " bad t-shirt",
-            brand: "puma",
-            category: "P",
-            price: 10.00,
-            country: "França"
-        }
-    ]
+
+    useEffect(() => {
+        const URL = "http://localhost:5000/products";
+
+        const promisse = axios.get(URL);
+
+        promisse.then((res) => {
+            setProducts(res.data);
+            console.log(res.data);
+        })
+        promisse.catch((err) => {
+            console.log(err.response.message);
+        })
+
+    },[])
 
     function addToCart(e) {
    
@@ -62,13 +49,12 @@ export default function HomePage() {
 
     return (
         <>
-            <GlobalHeader></GlobalHeader>
             <MainContent>
 
-                {arrayToTest.map((array, index) => {
+                {products.map((array, index) => {
                     return (
                         <>
-                            <ProductContainer key={index} id={array.id} brand={array.brand} productName={array.productName} price={array.price} description={array.description} category={array.category} country={array.country} />
+                            <ProductContainer key={index} id={array.id} brand={array.brand} image={array.image} productName={array.productName} price={array.price} description={array.description} category={array.category} country={array.country} />
                         </>
                     )
                 })
@@ -95,7 +81,6 @@ const MainContent = styled.main`
     justify-content: flex-start;
     overflow: auto;
 `
-
 const FooterContainer = styled.footer`
     width: 100%;
     height: 80px;
